8f1ff825c9346bf7a91969c6908505385e002f3d
deevkv <deev.kv@gmail.com>
2019-09-02 01:12:28 +0300

    Validation check. Change background to all permissions. Configure hover for button in prices. Styles for buttons up. Hover for the menu.

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -12,3 +12,12 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 })
+
+window.addEventListener('resize', function(event){
+  if(document.documentElement.clientWidth > 1200) {
+    if (navMain.classList.contains('main-nav--opened')) {
+    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--closed');
+  	}
+  }
+});
