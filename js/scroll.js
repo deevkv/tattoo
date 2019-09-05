10b9c5e4cdf8cedebaa81d04094c1e81477047f5
deevkv <deev.kv@gmail.com>
2019-09-05 18:58:14 +0300

    Build

--- js/scroll.js
+++ js/scroll.js
@@ -1,9 +1,9 @@
-const anchors = document.querySelectorAll('a[href*="#"]');
+const anchors = document.querySelectorAll('a[href^="#"]');  /*a[href^="#"]*/
 
 for (let anchor of anchors) {
 	anchor.addEventListener("click", function(event) {
 		event.preventDefault();
-		const blockID = anchor.getAttribute('href')
+		const blockID = anchor.getAttribute('href');
 		document.querySelector('' + blockID).scrollIntoView( {
 			behavior: "smooth",
 			block: "start"
