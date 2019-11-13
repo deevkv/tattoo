35b1199cd19005c21eaa951b5c58184877e28142
deevkv <deev.kv@gmail.com>
2019-11-13 10:53:53 +0300

    Scroll prohibition with modal window open. Corrects for video autorun

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,9 +1,11 @@
 const youTubeButtonPlay = document.querySelector('.about__button-play');
 const modalYouTube = document.querySelector('.modal-youtube');
+const bodyClass = document.querySelector('body'); 
 
 youTubeButtonPlay.addEventListener('click', function() {
 	event.preventDefault();
 	if (modalYouTube.classList.contains('modal-youtube--closed')) {
+    bodyClass.classList.add('modal-youtube--hidden');
     modalYouTube.classList.remove('modal-youtube--closed');
     modalYouTube.classList.add('modal-youtube--opened');
   } else {
@@ -15,6 +17,7 @@ youTubeButtonPlay.addEventListener('click', function() {
 
 modalYouTube.addEventListener('click', function() {
 	if (modalYouTube.classList.contains('modal-youtube--opened')) {
+    bodyClass.classList.remove('modal-youtube--hidden');
     modalYouTube.classList.remove('modal-youtube--opened');
     modalYouTube.classList.add('modal-youtube--closed');
     pauseVideo(); //in file player.js
